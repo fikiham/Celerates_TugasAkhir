e25c35301d3c678883ea44a82eb3d68351dfb131
Nurfan <129231001+Nurfan221@users.noreply.github.com>
2024-05-26 02:58:09 +0700

    update farm
    tingal benerin ItemDropInteractable blm bisa masukin item ke inventory

--- Assets/Script/Interactables/ItemDropInteractable.cs
+++ Assets/Script/Interactables/ItemDropInteractable.cs
@@ -1,14 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDropInteractable : Interactable
 {
     public Item item;
+    private Rigidbody2D rb;
+    public float gravityDuration = 1f; // Durasi gravitasi setelah item jatuh
+
+    private void Start()
+    {
+        Debug.Log("ItemDropInteractable: Start method called.");
+        rb = GetComponent<Rigidbody2D>(); // Inisialisasi Rigidbody2D
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>(); // Tambahkan Rigidbody2D jika belum ada
+            Debug.Log("ItemDropInteractable: Rigidbody2D component added.");
+        }
+    }
+
+     private void Update()
+    {
+        
+        promptMessage = "Panen Tanaman";
+
+    }
 
     protected override void Interact()
     {
-        Player_Inventory.Instance.AddItem(ItemPool.Instance.GetItem(item.itemName));
-        
+        Debug.Log("item di ambil.");
+        // Player_Inventory.Instance.AddItem(ItemPool.Instance.GetItem(item.itemName));
+        // Destroy(gameObject);
+    }
+
+    public IEnumerator StopGravity(Rigidbody2D rb, float delay)
+    {
+        Debug.Log("ItemDropInteractable: StopGravity coroutine started.");
+        yield return new WaitForSeconds(delay);
+        rb.gravityScale = 0;
+        rb.velocity = Vector2.zero;
+        Debug.Log("ItemDropInteractable: Gravity set to 0.");
     }
 }
